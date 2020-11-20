730a541058e647a8104b584f0b498746dd9065c6
Andrija <andreccccc12@gmail.com>
2020-11-20 15:40:58 +0100

    :tada: First commit

---
+++ main/Listic.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data.Common;
+using System.Text;
+
+namespace main
+{
+    class Listic
+    {
+        string ime_ucenika;
+        string prezime_ucenika;
+        byte redni_broj_ucenika;
+        string ime_razrednog_staresine;
+        string prezime_razrednog_staresine;
+
+        public Listic()
+        {
+            ime_ucenika = "";
+            prezime_ucenika = "";
+            redni_broj_ucenika = 0;
+            ime_razrednog_staresine = "";
+            prezime_razrednog_staresine = "";
+        }
+
+        public Listic(string ime_ucenika, string prezime_ucenika, byte redni_broj_ucenika, 
+            string ime_razrednog_staresine, string prezime_razrednog_staresine)
+        {
+            this.ime_ucenika = ime_ucenika;
+            this.prezime_ucenika = prezime_ucenika;
+            this.redni_broj_ucenika = redni_broj_ucenika;
+            this.ime_razrednog_staresine = ime_razrednog_staresine;
+            this.prezime_razrednog_staresine = prezime_razrednog_staresine;
+        }
+
+        public void StampajInfo()
+        {
+            Console.WriteLine("Ime ucenika: {0}\nPrezime ucenika: {1}\nRedni broj ucenika:{2}\nIme razrednog: {3}\nPrezime razrednog: {4}\n", this.ime_ucenika, this.prezime_ucenika, this.redni_broj_ucenika, this.ime_razrednog_staresine, this.prezime_razrednog_staresine);
+        }
+    }
+}
---
+++ main/Program.cs
@@ -0,0 +1,25 @@
+﻿using System;
+
+namespace main
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Unesi ime ucenika: ");
+            string ime_ucenika = Console.ReadLine();
+            Console.Write("Unesi prezime ucenika: ");
+            string prezime_ucenika = Console.ReadLine();
+            Console.Write("Unesi redni broj ucenika: ");
+            byte redni_broj_ucenika = Byte.Parse(Console.ReadLine());
+            Console.Write("Unesi ime razrednog staresine: ");
+            string ime_razrednog_staresine = Console.ReadLine();
+            Console.Write("Unesi prezime razrednog staresine: ");
+            string prezime_razrednog_staresine = Console.ReadLine();
+            Listic neki = new Listic(ime_ucenika, prezime_ucenika, redni_broj_ucenika, ime_razrednog_staresine, prezime_razrednog_staresine);
+            neki.StampajInfo();
+
+            Console.ReadKey();
+        }
+    }
+}
