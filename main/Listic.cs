b294a333a7390d773bb5fa7f2920d8d4e638ab9c
Andrija <andreccccc12@gmail.com>
2020-11-20 16:46:28 +0100

    Added creating student's paper functionality

--- main/Listic.cs
+++ main/Listic.cs
@@ -36,5 +36,81 @@ namespace main
         {
             Console.WriteLine("Ime ucenika: {0}\nPrezime ucenika: {1}\nRedni broj ucenika:{2}\nIme razrednog: {3}\nPrezime razrednog: {4}\n", this.ime_ucenika, this.prezime_ucenika, this.redni_broj_ucenika, this.ime_razrednog_staresine, this.prezime_razrednog_staresine);
         }
+
+        string[] ocene_srpski = new string[4];
+        string[] ocene_engleski = new string[4];
+        string[] ocene_fizicko = new string[4];
+        string[] ocene_matematika = new string[4];
+        string[] ocene_sociologija = new string[4];
+        string[] ocene_programiranje = new string[4];
+        string[] ocene_sistemi = new string[4];
+        string[] ocene_web = new string[4];
+        string[] ocene_mreze = new string[4];
+        string[] ocene_baze = new string[4];
+        string[] ocene_izborni = new string[4];
+        string nebitni_predmet;
+
+        public void UnesiOcene()
+        {
+            Console.Write("Unesi ocene za srpski: ");
+            this.ocene_srpski = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za engleski: ");
+            this.ocene_engleski = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za fizicko: ");
+            this.ocene_fizicko = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za matematiku: ");
+            this.ocene_matematika = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za sociologiju: ");
+            this.ocene_sociologija = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za programiranje: ");
+            this.ocene_programiranje = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za sisteme: ");
+            this.ocene_sistemi = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za web: ");
+            this.ocene_web = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za mreze: ");
+            this.ocene_mreze = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za baze: ");
+            this.ocene_baze = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za izborni: ");
+            this.ocene_izborni = Console.ReadLine().Split(' ');
+            Console.Write("Unesi ocene za nebitan predmet: ");
+            this.nebitni_predmet = Console.ReadLine();
+        }
+
+        public void IspisOcena()
+        {
+            Ispis(ocene_srpski);
+            Ispis(ocene_engleski);
+            Ispis(ocene_fizicko);
+            Ispis(ocene_matematika);
+            Ispis(ocene_sociologija);
+            Ispis(ocene_programiranje);
+            Ispis(ocene_sistemi);
+            Ispis(ocene_web);
+            Ispis(ocene_mreze);
+            Ispis(ocene_baze);
+            Ispis(ocene_izborni);
+            Console.WriteLine("Ocena iz nebitnog predmeta: {0}", nebitni_predmet);
+        }
+
+        string[] predmeti = new string[12] { "Srpski jezik i književnost", "Engleski jezik", "Fizičko vaspitanje",
+        "Matematika", "Sociologija sa pravima građana", "Programiranje", "Računarski sistemi", "Veb programiranje",
+        "Računarske mreže i internet servisi", "Baze podataka", "Izborni predmeti", " Građansko vaspitanje/Verska nastava"};
+
+        // globalni brojac za iteraciju kroz niz predmeti u Ispis metodi
+        int br = 0;
+
+        private void Ispis(string[] predmet)
+        {
+            Console.WriteLine("Ocene iz predmeta {0}", predmeti[this.br]);
+            this.br++;
+            foreach(string ocena in predmet)
+            {
+                Console.Write("{0} ", ocena);
+            }
+
+            Console.WriteLine();
+        }
     }
 }
--- main/Program.cs
+++ main/Program.cs
@@ -6,18 +6,47 @@ namespace main
     {
         static void Main(string[] args)
         {
-            Console.Write("Unesi ime ucenika: ");
-            string ime_ucenika = Console.ReadLine();
-            Console.Write("Unesi prezime ucenika: ");
-            string prezime_ucenika = Console.ReadLine();
-            Console.Write("Unesi redni broj ucenika: ");
-            byte redni_broj_ucenika = Byte.Parse(Console.ReadLine());
-            Console.Write("Unesi ime razrednog staresine: ");
-            string ime_razrednog_staresine = Console.ReadLine();
-            Console.Write("Unesi prezime razrednog staresine: ");
-            string prezime_razrednog_staresine = Console.ReadLine();
-            Listic neki = new Listic(ime_ucenika, prezime_ucenika, redni_broj_ucenika, ime_razrednog_staresine, prezime_razrednog_staresine);
-            neki.StampajInfo();
+            Console.WriteLine("Dobrodosli u program, unesite C da napravite novi listic, R da pronadjate postojeci, U da updatujete postojeci, D da obrisete postojeci");
+            biranje_moda:
+            char komanda = char.Parse(Console.ReadLine());
+            switch(komanda)
+            {
+                case 'C':
+                case 'c':
+                    Console.Write("Unesi ime ucenika: ");
+                    string ime_ucenika = Console.ReadLine();
+                    Console.Write("Unesi prezime ucenika: ");
+                    string prezime_ucenika = Console.ReadLine();
+                    Console.Write("Unesi redni broj ucenika: ");
+                    byte redni_broj_ucenika = byte.Parse(Console.ReadLine());
+                    Console.Write("Unesi ime razrednog staresine: ");
+                    string ime_razrednog_staresine = Console.ReadLine();
+                    Console.Write("Unesi prezime razrednog staresine: ");
+                    string prezime_razrednog_staresine = Console.ReadLine();
+                    Listic novi_listic = new Listic(ime_ucenika, prezime_ucenika, redni_broj_ucenika, ime_razrednog_staresine, prezime_razrednog_staresine);
+                    Console.Write("\n\n");
+                    novi_listic.StampajInfo();
+                    novi_listic.UnesiOcene();
+                    Console.Write("\n\n");
+                    novi_listic.IspisOcena();
+                    break;
+                case 'R':
+                case 'r':
+                    Console.WriteLine("Usli ste u read mode");
+                    break;
+                case 'U':
+                case 'u':
+                    Console.WriteLine("Usli ste u update mode");
+                    break;
+                case 'D':
+                case 'd':
+                    Console.WriteLine("Usli ste u delete mode");
+                    break;
+                default:
+                    Console.WriteLine("Uneli ste nevazecu komandu");
+                    Console.Write("Pokusajte ponovo: ");
+                    goto biranje_moda;
+            }
 
             Console.ReadKey();
         }
